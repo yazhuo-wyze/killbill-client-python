9f206f1afcdd8a7b397b4b08ef36109406a1ccfb
YazhuoDong <106308779+yazhuo-wyze@users.noreply.github.com>
2024-06-05 12:44:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "killbill"
-VERSION = "0.0.1-SNAPSHOT"
+# VERSION = "0.0.1-SNAPSHOT"
+VERSION = "0.0.1"
 # To install the library, run the following
 #
 # python setup.py install
